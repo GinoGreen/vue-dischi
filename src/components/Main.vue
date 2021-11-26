620c4877b4f6a671b2543da17b16be88b5ee5280
GinoGreen <luigiverde97@gmail.com>
2021-11-26 17:13:48 +0100

    Bonus loading

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,38 +1,53 @@
 <template>
    <main>
-      <div class="myContainer">
+
+      <!-- loading -->
+      <div v-if="isLoading" class="loading m-5">
+         <Loading
+            title="Caricamento albums..."
+         />
+      </div>
+
+      <!-- albums -->
+      <div v-else class="myContainer">
          <Album
             v-for="(album, index) in albums" :key="index"
             :album="album"
          />
       </div>
+
    </main>
 </template>
 
 <script>
 
 import axios from 'axios';
+import Loading from './Loading.vue';
 import Album from './Album.vue';
 
 export default {
    name: 'Main',
    components: {
-      Album
+      Album,
+      Loading
    },
    data() {
       return {
          albums: [],
+         isLoading: false,
          apiURL: 'https://flynn.boolean.careers/exercises/api/array/music'
       }
    },
    methods: {
       getApi() {
+
+         this.isLoading = true;
+
          axios.get(this.apiURL)
             .then( response => {
 
                this.albums = response.data.response;
-               console.log(this.albums);
-
+               this.isLoading = false;
             })
             .catch( error => {
                console.log(error);
@@ -50,14 +65,20 @@ export default {
 @import '../assets/style/vars.scss';
 @import '../assets/style/mixins.scss';
 
+$loading: 'true';
+
    main {
       height: calc(100% - 80px);
 
-      @include center();
+      @if $loading == 'false' { @include center(); }
+      @else { @include center('justify') }
 
       overflow-y: auto;
       overflow-x: hidden;
-      background-color: $primary-color;
+      background-image: linear-gradient(
+         lighten($primary-color, 4%),
+         lighten($primary-color, 0%)
+         );
 
       .myContainer {
          width: 1170px;
